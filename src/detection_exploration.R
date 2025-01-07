0cd77ac8f11165b3764dc70a798cd2ea157b222b
Gio Jacuzzi <giojacuzzi@gmail.com>
2025-01-07 15:53:06 -0800

    species presence and alpha diversity by site

--- src/detection_exploration.R
+++ src/detection_exploration.R
@@ -5,24 +5,37 @@ path_input_dir = "data/processed/predicted_detection_histories"
 
 ############################################################################################################
 
+library(tidyverse)
+
 paths_site = list.files(path_input_dir, full.names = TRUE)
 
-# Calculate naive species richness by site
-message('Calculating naive species richness by site...')
+# Calculate species presence and naive alpha diversity (species richness) by site
+message('Calculating species presence and naive alpha diversity (species richness) by site...')
 site_richness = tibble()
+site_species = tibble()
 
 for (path_site in paths_site) {
   # Load site detection history
   site_detections = read_csv(path_site, show_col_types = FALSE)
-  site_id = basename(path_site)
+  site_id = sub('.csv', '', basename(path_site))
   message('Site ', site_id)
   # Determine which species were detected during at least one visit at the site
   species_detected = site_detections %>% filter(rowSums(select(., -species)) >= 1) %>% select(species)
+  site_species = bind_rows(site_species, tibble(
+    site = site_id,
+    species = as.list(species_detected)
+  ))
   # Calculate richness
   site_richness = bind_rows(site_richness, tibble(
     site  = site_id,
     alpha = nrow(species_detected)
   ))
 }
-message('Species richness by site:')
+
+message('Species by site:')
+for (i in 1:nrow(site_species)) {
+  message(site_species$site[[i]], ' (', length(site_species$species[[i]]), ')')
+  print(site_species$species[[i]])
+}
+message('Alpha diversity (species richness) by site:')
 print(site_richness)
