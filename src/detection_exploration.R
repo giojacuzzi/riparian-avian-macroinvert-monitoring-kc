1440617d23ece273f41a631db1169731f5108a76
Gio Jacuzzi <giojacuzzi@gmail.com>
2025-01-08 12:57:18 -0800

    visualize data and provide example linear regression

--- src/detection_exploration.R
+++ src/detection_exploration.R
@@ -2,10 +2,13 @@
 
 # INPUT
 path_input_dir = "data/processed/predicted_detection_histories"
+path_site_data = "data/site_data.csv"
 
 ############################################################################################################
 
 library(tidyverse)
+library(ggplot2)
+theme_set(theme_classic())
 
 paths_site = list.files(path_input_dir, full.names = TRUE)
 
@@ -39,3 +42,42 @@ for (i in 1:nrow(site_species)) {
 }
 message('Alpha diversity (species richness) by site:')
 print(site_richness)
+p = ggplot(site_richness, aes(x = alpha)) +
+  geom_histogram() +
+  labs(title='Alpha diversity (species richness) distribution across sites'); print(p)
+
+############################################################################################################
+readline('Proceed with site data analysis? [enter]')
+
+# Load data per site
+site_data = read_csv(path_site_data, show_col_types = FALSE)
+
+# Visualize B-IBI across sites
+p = ggplot(site_data, aes(x = bibi_lmh)) +
+  geom_bar() +
+  labs(title='B-IBI across sites'); print(p)
+
+# Visualize impervious coverage across sites
+p = ggplot(site_data, aes(x = imp_lmh)) +
+  geom_bar() +
+  labs(title='Impervious coverage across sites'); print(p)
+
+message('Analyzing impervious coverage x mean B-IBI...')
+
+# Calculate Pearson's correlation coefficient for impervious coverage x mean B-IBI
+correlation_pearson = cor(site_data$local100_imp_coverage, site_data$mean_BIBI, method = "pearson")
+message("Pearson's correlation coefficient ", round(correlation_pearson, 2))
+
+# Fit a linear regression model
+model <- lm(site_data$mean_BIBI ~ site_data$local100_imp_coverage)
+summary(model)
+
+# Visualize impervious coverage x mean B-IBI
+p = ggplot(site_data, aes(x = local100_imp_coverage, y = mean_BIBI)) +
+  geom_point() +
+  stat_smooth(method = "lm",formula = y ~ x, geom = "smooth") + # linear regression
+  labs(title='Impervious coverage x mean B-IBI'); print(p)
+
+# TODO: Explore relationship between species richness and impervious coverage
+
+# TODO: Explore relationship between species richness and B-IBI
