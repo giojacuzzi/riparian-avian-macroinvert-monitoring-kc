f715ea434723c25b6937d9f11c835d6010ec90e4
Gio Jacuzzi <giojacuzzi@gmail.com>
2025-01-10 16:09:28 -0800

    refactor data directory and add todo in global.R

---
+++ src/global.R
@@ -0,0 +1,12 @@
+# TODO: Load all land cover and benthos data for each site and join with the site metadata to create a "master" data_habitat dataframe
+
+path_land_cover = "data/processed/land_cover/land_cover.csv" # processed land cover data from GIS
+path_benthos_raw = "data/raw/benthos/ScoresByYear.txt" # raw PSSB benthos data
+
+data_land_cover = read.csv(path_land_cover)
+data_benthos = read.table(path_benthos_raw, sep = "\t", header=TRUE, fill = TRUE)
+  
+metadata_site = read.csv('data/site_metadata.csv')
+  
+data_habitat = full_join(data_land_cover, metadata_site, by = c("site" = "aru_site"))
+data_habitat = left_join(data_habitat, data_benthos, by = c("benthos_site" = "Site.ID"))
